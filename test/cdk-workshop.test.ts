16b5175d9f0dde2ef9da9973e455149c4689e289
kPOWz <karrie.zeman@gmail.com>
2021-02-27 20:38:04 -0600

    hello lambda

--- test/cdk-workshop.test.ts
+++ test/cdk-workshop.test.ts
@@ -1,21 +1,10 @@
 import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as CdkWorkshop from '../lib/cdk-workshop-stack';
-
-test('SQS Queue Created', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new CdkWorkshop.CdkWorkshopStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(haveResource("AWS::SQS::Queue",{
-      VisibilityTimeout: 300
-    }));
-});
-
-test('SNS Topic Created', () => {
+test('Lambda Function Created', () => {
   const app = new cdk.App();
   // WHEN
   const stack = new CdkWorkshop.CdkWorkshopStack(app, 'MyTestStack');
   // THEN
-  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+  expectCDK(stack).to(haveResource("AWS::Lambda::Function"));
 });
